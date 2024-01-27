9e26eb1a7f22dfb8693014070d6665b3ea40b14a
unknown <kariukibenard189@gmail.com>
2024-01-27 14:24:41 +0300

    Added the CORS middleware so I could access my API with a frontend running on a different domain.

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require("express");
 
 const db = require("./data/database");
 const todoRoutes = require("./routes/todo-routes");
+const corsMiddleware = require("./middlewares/cors");
 
 const app = express();
+
+app.use(corsMiddleware);
 
 app.use(express.json());
 
 app.use(todoRoutes);
 
 app.use(function (error, req, res, next) {
-  console.log(error)
+  console.log(error);
   res.status(500).json({
     message: "Something went wrong!",
   });
